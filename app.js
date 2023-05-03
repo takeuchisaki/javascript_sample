e13752e7af33d9df2aeb935ed0a03d333576a2e0
takeuchisaki <129586704+takeuchisaki@users.noreply.github.com>
2023-05-03 03:32:40 +0000

    js-2 条件分岐

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert('Hello World');
+let orange = 100;
+let apple = 120;
+
+if(orange<apple){
+  alert('みかんの値段がりんごより安い');
+}else if (orange == apple){
+  alert('みかんとりんごが同じ値段');
+}else{
+  alert('みかんの値段がりんごより高い');
+}
